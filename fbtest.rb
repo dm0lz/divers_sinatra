1b31e089da521d5f0df0c4524e3490bd4017d8ae
OlIvIeR77 <ducrouxolivier@gmail.com>
2012-10-17 23:09:33 -0300

    ii

--- fbtest.rb
+++ fbtest.rb
@@ -6,20 +6,20 @@ require 'sinatra'
 authorize_url = "https://graph.facebook.com/oauth/authorize"
 access_token_url = "https://graph.facebook.com/oauth/access_token"
 
-fetcher_app_id = "???"
+your_app_id = "???"
 client_secret = "???"
 redirect_uri = "http://localhost:4567/oauth_callback"
 
 get "/" do
-  redirect "#{authorize_url}?client_id=#{fetcher_app_id}&redirect_uri=#{redirect_uri}"
+  redirect "#{authorize_url}?client_id=#{your_app_id}&redirect_uri=#{redirect_uri}"
 end
 
 get "/more" do
-  redirect "#{authorize_url}?client_id=#{fetcher_app_id}&redirect_uri=#{redirect_uri}&scope=user_photos,user_videos,publish_stream"
+  redirect "#{authorize_url}?client_id=#{your_app_id}&redirect_uri=#{redirect_uri}&scope=user_photos,user_videos,publish_stream"
 end
 
 get "/oauth_callback" do
-  redirect "#{access_token_url}?client_id=#{fetcher_app_id}&redirect_uri=#{redirect_uri}&client_secret=#{client_secret}&code=#{params[:code]}"
+  redirect "#{access_token_url}?client_id=#{your_app_id}&redirect_uri=#{redirect_uri}&client_secret=#{client_secret}&code=#{params[:code]}"
   #guardar en la bd el acces_token
   params[:access_token]
 end
